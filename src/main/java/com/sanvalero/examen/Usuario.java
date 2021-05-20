72d2533c42dba5d1ad9d2222f7fa9283214a91f5
Sandra_SFR <sandrafantonirubio@gmail.com>
2021-05-20 19:47:26 +0200

    Clase usuario

---
+++ src/main/java/com/sanvalero/examen/Usuario.java
@@ -0,0 +1,43 @@
+package com.sanvalero.examen;
+
+public class Usuario {
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String contrasena;
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
